d6b1403538cfd0a48cbe8fd425ca11d5376dfe18
963461117@qq.com <963461117@qq.com>
2018-07-05 21:13:07 +0800

    texttaaaaaaa

--- app/src/main/java/com/example/zdm/weiyingdemo/view/MainActivity.java
+++ app/src/main/java/com/example/zdm/weiyingdemo/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.zdm.weiyingdemo;
+package com.example.zdm.weiyingdemo.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.zdm.weiyingdemo.R;
 
 public class MainActivity extends AppCompatActivity {
 
