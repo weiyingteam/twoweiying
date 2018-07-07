6a26c3e5f353321dfdba73638b57673b48ed9f2d
min1215 <473281070@qq.com>
2018-07-07 12:06:16 +0800

    11

---
+++ app/src/main/java/com/example/zdm/weiyingdemo/view/costom/TitleLayout.java
@@ -0,0 +1,56 @@
+package com.example.zdm.weiyingdemo.view.costom;
+
+import android.app.Activity;
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.zdm.weiyingdemo.R;
+
+public class TitleLayout extends LinearLayout {
+
+    private TextView tv_title;
+    public TitleLayout(Context context) {
+        super(context,null);
+    }
+
+    public TitleLayout(final Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        //引入布局
+        View inflate = LayoutInflater.from(context).inflate(R.layout.title_bar, this);
+        Button btnBack=(Button)inflate.findViewById(R.id.btnBack);
+        Button btnEdit=(Button)inflate.findViewById(R.id.btnEdit);
+        btnBack.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((Activity)getContext()).finish();
+            }
+        });
+
+        btnEdit.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context,"编辑",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        tv_title=(TextView)findViewById(R.id.tv_title);
+
+    }
+
+    //显示活的的标题
+    public void setTitle(String title)
+    {
+        if(!TextUtils.isEmpty(title))
+        {
+            tv_title.setText(title);
+        }
+    }
+}
