a20e071e29ef3347431fe17035cd5c5682dbe76d
min1215 <473281070@qq.com>
2018-07-13 20:06:40 +0800

    2525626

--- app/src/main/java/com/example/zdm/weiyingdemo/model/http/RetrofitApi.java
+++ app/src/main/java/com/example/zdm/weiyingdemo/model/http/RetrofitApi.java
@@ -27,23 +27,7 @@ public interface RetrofitApi {
     @POST("front/columns/getVideoList.do")
     @FormUrlEncoded
     Observable<VideoListBean> getVideoList(@Field("catalogId") String catalogId, @Field("information") String information);
-//http://api.svipmovie.com/front/columns/getVideoList.do?catalogId=402834815584e463015584e53843000b&information=null
-//    1.首页接口
-    //http://api.svipmovie.com/front/homePageApi/homePage.do
-
-
-//    2. 频道列表
-//    接口地址：http://api.svipmovie.com/columns/getVideoList.do 
-
-
-//    3.详情接口
-//    接口地址：http://api.svipmovie.com/front/videoDetailApi/videoDetail.do
-
-
-//    4.获取用户信息
-//    接口地址： http://api.svipmovie.com/front/searchKeyWordApi/getVideoListByKeyWord.do 
-
-
-//            5.评论列表接口
-//    接口地址：http://api.svipmovie.com/front/Commentary/getCommentList.do?mediaId=CMCC_00000000000000001_621653189
+    @POST("front/Commentary/getCommentList.do")
+    @FormUrlEncoded
+    Observable<ConstantBean> contant(@Field("mediaId") String mediaId);
 }
