875df41e9edb73398f6692498056d063ac282ad0
402652568 <402652568@qq.com>
2018-07-13 20:05:02 +0800

    e88

--- app/src/main/java/com/example/zdm/weiyingdemo/model/http/RetrofitApi.java
+++ app/src/main/java/com/example/zdm/weiyingdemo/model/http/RetrofitApi.java
@@ -1,8 +1,6 @@
 package com.example.zdm.weiyingdemo.model.http;
 
-import com.example.zdm.weiyingdemo.app.Constant;
 import com.example.zdm.weiyingdemo.model.bean.AbBean;
-import com.example.zdm.weiyingdemo.model.bean.ConstantBean;
 import com.example.zdm.weiyingdemo.model.bean.VideoDateilsBean;
 import com.example.zdm.weiyingdemo.model.bean.VideoListBean;
 
@@ -27,23 +25,4 @@ public interface RetrofitApi {
     @POST("front/columns/getVideoList.do")
     @FormUrlEncoded
     Observable<VideoListBean> getVideoList(@Field("catalogId") String catalogId, @Field("information") String information);
-//http://api.svipmovie.com/front/columns/getVideoList.do?catalogId=402834815584e463015584e53843000b&information=null
-//    1.首页接口
-    //http://api.svipmovie.com/front/homePageApi/homePage.do
-
-
-//    2. 频道列表
-//    接口地址：http://api.svipmovie.com/columns/getVideoList.do 
-
-
-//    3.详情接口
-//    接口地址：http://api.svipmovie.com/front/videoDetailApi/videoDetail.do
-
-
-//    4.获取用户信息
-//    接口地址： http://api.svipmovie.com/front/searchKeyWordApi/getVideoListByKeyWord.do 
-
-
-//            5.评论列表接口
-//    接口地址：http://api.svipmovie.com/front/Commentary/getCommentList.do?mediaId=CMCC_00000000000000001_621653189
 }
